5aff7da19739c2cbc702d2367801ec2be1971967
Beata Kosmider <kosmibe@gmail.com>
2023-02-25 09:18:21 +0100

    it works but test fails

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Twoj kod tutaj
+
+const btn = document.querySelector("#startTimeout");
+btn.addEventListener("click", function () {
+  setTimeout(() => {
+    const h1 = document.createElement("h1");
+    h1.innerText = "Boom!";
+    document.body.appendChild(h1);
+  }, 5 * 1000);
+});
